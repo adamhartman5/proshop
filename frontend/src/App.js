f989e017f91a6e91353f7f219c8b2b869f84ecff
Adam Hartman <adamhartman5@gmail.com>
2023-06-19 01:36:47 -0400

    Create navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
 const App = () => {
-  return (
-    <h1>Welcome to ProShop</h1>
-  )
-}
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<h1>Welcome to ProShop</h1>
+			</main>
+		</>
+	);
+};
 
-export default App
+export default App;
